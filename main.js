707e6569356d53a881c0738f2a314510aec551f6
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2024-09-18 12:11:03 +0530

    Adding my latest projects details

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["Frontend Developer", "Web Developer", "Android Developer"],
+    strings:["Frontend Developer", "Web Developer", "Android Developer", "Fullstack Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
